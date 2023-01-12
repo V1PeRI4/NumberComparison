6222c21b56d13ce0231bff1b93d25c728dc48750
V1PeRI4 <komardenkovt@gmail.com>
2023-01-12 13:51:46 +0300

    Добавьте файлы проекта.

---
+++ Example/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 числа");
+            string firstNumString = Console.ReadLine();
+            string secondNumString = Console.ReadLine();
+
+
+            try
+            {
+                float firstNum = float.Parse(firstNumString);
+                float secondNum = float.Parse(secondNumString);
+
+
+                if (firstNum == secondNum)
+                    Console.WriteLine($"{firstNum} = {secondNum}");
+                else if (firstNum > secondNum)
+                    Console.WriteLine($"{firstNum} > {secondNum}");
+                else
+                    Console.WriteLine($"{secondNum} > {firstNum}");
+
+               
+            }
+            catch (FormatException e)
+            { 
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
